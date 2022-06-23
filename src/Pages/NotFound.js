756a3c03659cec39329549dc065fa0efd4b29384
MridulTi <mridultiwari2002@gmail.com>
2022-06-23 16:11:23 +0530

    Added Structure of the page

---
+++ src/Pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react"
+export default function NotFound(){
+    return(
+        <div>
+            <h1>NOT FOUND</h1>
+            <h3>Error 404</h3>
+            <hr></hr>
+            <h5>The named page was not found</h5>
+        </div>
+    )
+}
