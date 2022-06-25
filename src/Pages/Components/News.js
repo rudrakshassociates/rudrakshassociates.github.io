3e7ae40372d5ce68f2939370669604e24e8da62b
MridulTi <mridultiwari2002@gmail.com>
2022-06-25 11:22:39 +0530

    Added Different Pages

---
+++ src/Pages/Components/News.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {Link} from "react-router-dom"
+import "./styles/Navbar.css"
+export default function News(){
+    return (
+        <div>
+           
+        </div>
+    )
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,32 @@
 import React from "react"
+import {Link} from "react-router-dom"
 import Navbar from "./Components/Navbar.js"
+import "./Components/styles/home.css"
+import CEO from "./Components/CEO.js"
+import Footer from "./Components/Footer.js"
+import Gallery from "./Components/Gallery.js"
+import News from "./Components/News.js"
 export default function Home(){
     return(
         <div>
             <Navbar/>
+            <div className="Home-bg">
+                <img src="Images/background-head1.jpg"/>
+                <div className="Title">
+                    <h1>RUDRAKSH ASSOCIATES</h1>
+                    <Link to="/contactus"><button className="GetinTouch">Get in Touch</button></Link>
+                </div>
+            </div>
+            <div className="our-services">
+                <h1>Our Services</h1>
+                <hr></hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
+            </div>
+            <CEO/>
+            <Gallery/>
+            <News/>
+            <Footer/>
+            
         </div>
     )
 }
