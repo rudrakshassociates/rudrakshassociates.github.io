c8603eabf5bd1b67ffef152493ba52d19e0cf70d
MridulTi <mridultiwari2002@gmail.com>
2022-07-02 08:09:25 +0530

    Changed Contact Page content and style

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -5,39 +5,58 @@ import "./Components/styles/contactus.css"
 export default function ContactUs(){
     return(
         <div>
-            <Navbar/>
-            <div className="CU-Container">
-                <div className="CU-Header">
-                    <img src="Images/contact-head.jpeg"/>
-                </div>
-                <div className="CU-Content">
-                    <div className="CU-para">
-                        <h2>Get in Touch</h2>
-                        <h3>Wanna work with us?</h3>
+            <div className="main">
+                <Navbar/>
+                <div className="CU-Container">
+                    <div className="CU-Header">
+                        <h2>We love meeting new people and helping them solve complex design challenges.</h2>
                     </div>
-                    <div className="CU-form">
-                        <form>
-                            
-                            <input
+                    <div className="CU-Content">
+                        <div className="image-replacement-1">Lorem Ipsum-Lanscape</div>
+                        <div className="image-replacement-2">Lorem Ipsum Portrait</div>
+                        <div className="CU-para">
+                            <h2><b>PHONE: </b>+91 9818321006 , +91 9810536966</h2>
+                            <h2><b>EMAIL : </b>adhirajconstructions2018@gmail.com</h2>
+                        </div>
+                        
+                        <div className="CU-form">
+                            <form>
+                                <div>
+                                    <input
+                                    type="text"
+                                    placeholder="First Name"
+                                    />
+                                    <input
+                                    type="text"
+                                    placeholder="Last Name"
+                                    />
+                                    </div>
+                                
+                                <input
+                                
                                 type="text"
-                                placeholder="Name"
-                            />
-                            <input
-                                type="text"
-                                placeholder="Email"
-                            />
-                            <textarea
-                                type="text"
-                                placeholder="Message"
-                            ></textarea>
-                            <br></br>
-                            <button>SEND</button>
+                                placeholder="Phone Number"
+                                />
+                                <input
+                                
+                                    type="text"
+                                    placeholder="Email"
+                                />
+                                <textarea
+                                    type="text"
+                                    placeholder="Message"
+                                ></textarea>
+                                <br></br>
+                                <button>Get in Touch</button>
 
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <div className="footer">
+                <Footer/>
+            </div>
         </div>
     )
 }
